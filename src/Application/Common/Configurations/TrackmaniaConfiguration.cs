5c81fa6941581ca061618ea2532cd92f3e98be76
florian faity <florian.faity@hotmail.fr>
2024-03-25 23:10:00 +0100

    setting

--- src/Application/Common/Configurations/TrackmaniaConfiguration.cs
+++ src/Application/Common/Configurations/TrackmaniaConfiguration.cs
@@ -3,6 +3,8 @@ namespace tm20trial.Application.Common.Configurations;
 public class TrackmaniaConfiguration
 {
     public required string ClientId { get; set; }
+    
+    // public required string ClientSecret { get; set; }
 
     public required string BaseLoginURL { get; set; }
 
