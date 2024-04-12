ad4275b0cb2eee91907519ba49a21852ee7cf31e
Florian FAITY <florian.faity@mcsa.fr>
2024-04-12 14:03:17 +0200

    List map

--- src/Web/ClientApp/src/app/maps/maps-list/maps-list.component.ts
+++ src/Web/ClientApp/src/app/maps/maps-list/maps-list.component.ts
@@ -1,10 +1,12 @@
 
 import {gridResponsiveMap, NzBreakpointService} from "ng-zorro-antd/core/services";
-import {Subject, takeUntil} from "rxjs";
-import {EDifficulty, EStateValidation, MapDto, MapsClient} from "../../web-api-client";
+import {shareReplay, Subject, takeUntil} from "rxjs";
+import {CurrentUserDto, EDifficulty, EStateValidation, MapDto, MapsClient} from "../../web-api-client";
 import {ToastService} from "../../shared/services/toast.service";
 import {Component, OnDestroy} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
+import {HttpErrorResponse} from "@angular/common/http";
+import {AuthorizeService} from "../../../api-authorization/authorize.service";
 
 @Component({
   selector: 'app-maps-list',
@@ -21,12 +23,15 @@ export class MapsListComponent implements OnDestroy {
   currentBreakpoint: string = '';
   enumDifficulty = EDifficulty;
 
+  isMapper: boolean = false;
+
 
   constructor(private mapsClient: MapsClient,
               private _route: ActivatedRoute,
               private _router: Router,
               private _toastService: ToastService,
-              private _nzBreakpoint: NzBreakpointService
+              private _nzBreakpoint: NzBreakpointService,
+              private _authorizeService: AuthorizeService,
   ) {
     this.loading = true;
     mapsClient.getMaps(EStateValidation.Validate).subscribe({
@@ -45,6 +50,18 @@ export class MapsListComponent implements OnDestroy {
       .subscribe((c) => {
         this.currentBreakpoint = c;
       });
+
+
+    this._authorizeService.getUser().pipe(shareReplay()).subscribe({
+      next: (user: CurrentUserDto) => {
+        this.isMapper = user.roles.indexOf('Administrator') > -1 || user.roles.indexOf('Mapper') > -1;
+      },
+      error: (err: HttpErrorResponse) => {
+        if (err.status == 401) {
+          this.isMapper = false;
+        }
+      }
+    });
   }
 
   listMapsByDifficulty(difficulty: EDifficulty){
@@ -72,4 +89,8 @@ export class MapsListComponent implements OnDestroy {
     this.destroy$.complete();
   }
 
+
+  goToSuggestMap(){
+    console.log("goToSuggestMap");
+  }
 }
